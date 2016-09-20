ba9baa8ba20e6337f3489bb575921fa191e9b49b
Isaac Avram <Isaac Avram>
2016-09-19 19:38:15 -0700

    prepared for pip install

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pizza-utils",
+    version="0.0.1",
+    url="https://github.com/ILikePizza555/py-pizza-utils"
+    description="My personal python utilities",
+    author="Isaac Avram",
+    author_email="avrisaac555@gmail.com",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=find_packages(exclude=["tests"])
+)
